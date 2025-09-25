833580d86a212fc275638f11f9b71c3a60e06d72
Asep3580 <asep3580@gmail.com>
2025-09-25 11:55:49 +0700

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -2,8 +2,9 @@
  
  // Variabel ini akan diganti oleh Vercel saat proses build.
  // Pastikan nama variabelnya sama persis dengan yang ada di Vercel (case-sensitive).
- const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://kopkaka.onrender.com/api';
+ const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://www.kopkaka.com/api';
  
  // Export variabel agar bisa diimpor di file lain
 
  export { API_URL };
+
