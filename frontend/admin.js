57335359ccb84ee8c451064437cc20c45b3477dc
Asep3580 <asep3580@gmail.com>
2025-09-25 10:19:54 +0700

    Update admin.js

--- frontend/admin.js
+++ frontend/admin.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // --- KONFIGURASI & ELEMEN GLOBAL ---
-    const API_URL = 'https://kopkaka.onrender.com/'; // URL utama API
+    const API_URL = 'https://kopkaka.onrender.com/api'; // URL utama API
     const ADMIN_API_URL = `${API_URL}/admin`; // URL untuk endpoint admin
     const sidebar = document.getElementById('sidebar');
     const sidebarOverlay = document.getElementById('sidebar-overlay');
@@ -6083,3 +6083,4 @@ const renderCashFlowChart = (data) => {
     initializeApp();
 
 });
+
