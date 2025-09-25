8eb27c239a5593a99b66bcca7f4d0008e00373ca
Asep3580 <asep3580@gmail.com>
2025-09-25 12:39:59 +0700

    Add files via upload

--- backend/src/routes/partner.routes.js
+++ backend/src/routes/partner.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getPartners, getPublicPartners, createPartner, updatePartner, deletePartner, getPartnerById } = require('../controllers/partner.controller');
+const { getPartners, createPartner, updatePartner, deletePartner, getPartnerById } = require('../controllers/partner.controller');
 const authMiddleware = require('../middleware/auth.middleware');
 const authorize = require('../middleware/role.middleware');
 const { uploadPartnerLogo } = require('../middleware/upload');
@@ -8,9 +8,6 @@ const { uploadPartnerLogo } = require('../middleware/upload');
 // This permission will be used for admin-only routes.
 // The `authorize` middleware allows the 'admin' role to pass any check.
 const adminPermission = ['viewSettings'];
-
-// Public route
-router.get('/public/partners', getPublicPartners);
 
 // Admin routes
 router.route('/admin/partners')
